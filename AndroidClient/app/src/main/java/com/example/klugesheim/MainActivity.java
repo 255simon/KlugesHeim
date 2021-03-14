4a09307f3dc3fca19782f0f011dd8734e1546b74
Julian Brand <julian.brand@stud.hs-mannheim.de>
2021-03-14 18:25:47 +0100

    added provisionally scan button and text view for received message

--- AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ToggleButton lightButton = findViewById(R.id.button_light1);
+        final Button scanButton = findViewById(R.id.scan_button);
+        final TextView out = findViewById(R.id.output);
+
         lightButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 if(isChecked) {
@@ -29,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 else{
                     sendMessage("pilight-send -p clarus_switch -i A3 -u 20 --off");
                 }
+            }
+        });
+        scanButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View buttonView){
+                receiveMessage();
             }
         });
     }
@@ -53,6 +64,33 @@ public class MainActivity extends AppCompatActivity {
 
         thread.start();
     }
-    private String host = "";    //insert server ip here
+
+    private void receiveMessage(){
+        final TextView out = findViewById(R.id.output);
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+
+               try{
+                   Socket sock = new Socket(host, port);
+                   OutputStream outStream = sock.getOutputStream();
+                   PrintWriter output = new PrintWriter(outStream);
+                   output.print("scan");
+                   output.flush();
+                   in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                   String msg = in.readLine();
+                   out.setText(msg);
+                   sock.close();
+               }catch(IOException e){
+                   e.printStackTrace();
+               }
+           }
+
+        });
+
+        thread.start();
+    }
+    private String host = "192.168.2.200";    //insert server ip here
     private int port = 2048;
+    BufferedReader in;
 }
