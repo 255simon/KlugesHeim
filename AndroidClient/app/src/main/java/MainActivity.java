08939799dd8ad1c8514620a028cb69926e6d8a19
Julian Brand <julian.brand@stud.hs-mannheim.de>
2021-03-15 20:17:44 +0100

    add listview

--- AndroidClient/app/src/main/java/MainActivity.java
+++ AndroidClient/app/src/main/java/MainActivity.java
@@ -1,13 +1,15 @@
-package com.example.klugesheim;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.example.klugesheim.R;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +18,7 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,25 +26,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ToggleButton lightButton = findViewById(R.id.button_light1);
         final Button scanButton = findViewById(R.id.scan_button);
-        final TextView out = findViewById(R.id.output);
 
-        lightButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
-                if(isChecked) {
-                    sendMessage("pilight-send -p clarus_switch -i A3 -u 20 --on");
-                }
-                else{
-                    sendMessage("pilight-send -p clarus_switch -i A3 -u 20 --off");
-                }
-            }
-        });
         scanButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View buttonView){
                 receiveMessage();
             }
         });
+        ListView listView = (ListView) findViewById(R.id.listview_activity_main);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("whats");
+        arrayList.add("up");
+        ArrayAdapter<String> myArrayAdapter =
+                new ArrayAdapter<>(
+                  this,
+                  R.layout.list_row,
+                  R.id.textView,
+                  arrayList);
+        listView.setAdapter(myArrayAdapter);
     }
     private void sendMessage(final String msg) {
 
@@ -66,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void receiveMessage(){
-        final TextView out = findViewById(R.id.output);
+        //final TextView out = findViewById(R.id.output);
         Thread thread = new Thread(new Runnable(){
             @Override
             public void run(){
@@ -79,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                    output.flush();
                    in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
                    String msg = in.readLine();
-                   out.setText(msg);
+                   //out.setText(msg);
                    sock.close();
                }catch(IOException e){
                    e.printStackTrace();
@@ -90,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
 
         thread.start();
     }
+
     private String host = "";    //insert server ip here
     private int port = 2048;
     BufferedReader in;
