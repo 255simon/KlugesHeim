c8171db226d5356807ce85dae11db2d37cbb3381
Julian Brand <julian.brand@stud.hs-mannheim.de>
2021-03-16 00:13:11 +0100

    modify receiveMessage function

--- AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
@@ -4,14 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.ToggleButton;
 
-import com.example.klugesheim.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,14 +15,27 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    static private String host = "";    //insert server ip here
+    static private String host = "192.168.2.200";    //insert server ip here
     static private int port = 2048;
+    ArrayList<Switch> switchList;
+    SwitchAdapter switchAdapter;
     BufferedReader in;
+
+    private class Command{
+        private String command;
+
+        public String getCommand() {
+            return command;
+        }
+
+        public void setCommand(String command) {
+            this.command = command;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,34 +45,43 @@ public class MainActivity extends AppCompatActivity {
 
         scanButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View buttonView) {
-                receiveMessage();
+                try {
+                    receiveMessage();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
         ListView listView = (ListView) findViewById(R.id.listview_activity_main);
-        ArrayList<Switch> switchList = new ArrayList<Switch>();
+        switchList = new ArrayList<Switch>();
         Switch s1 = new Switch("Kühlschrank", "-p clarus_switch -i A3 -u 20");
         switchList.add(s1);
-        SwitchAdapter switchAdapter = new SwitchAdapter(getApplicationContext(), switchList);
+        switchAdapter = new SwitchAdapter(getApplicationContext(), switchList);
         listView.setAdapter(switchAdapter);
     }
 
-    private void receiveMessage(){
+    private void receiveMessage() throws InterruptedException {
         //final TextView out = findViewById(R.id.output);
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run(){
+        TextInputLayout textInput = findViewById(R.id.name_input_layout);
+        String switchName;
+        switchName = textInput.getEditText().getText().toString();
 
-                try{
+        final Command com = new Command();
+        Thread thread = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+
+                try {
                     Socket sock = new Socket(host, port);
                     OutputStream outStream = sock.getOutputStream();
                     PrintWriter output = new PrintWriter(outStream);
                     output.print("scan");
                     output.flush();
                     in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                    String msg = in.readLine();
-                    //out.setText(msg);
+                    com.setCommand(in.readLine());
                     sock.close();
-                }catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -71,6 +89,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         thread.start();
+        thread.join();
+        switchAdapter.add(new Switch(switchName, com.getCommand()));
+        switchAdapter.notifyDataSetChanged();
     }
 
     static public String getHost(){
