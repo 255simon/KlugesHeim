44e675fcec2f6328afbc9699109bab11bd02e1a1
Julian Brand <julian.brand@stud.hs-mannheim.de>
2021-03-16 12:41:20 +0100

    save data of listview elements in shared preferences

--- AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.klugesheim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.icu.text.Edits;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,16 +19,24 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     static private String host = "192.168.2.200";    //insert server ip here
     static private int port = 2048;
+    private String switchKey = "switch_names";
+    private String commandKey = "commands";
     ArrayList<Switch> switchList;
     SwitchAdapter switchAdapter;
+    SharedPreferences sharedPref;
+    HashSet<String> switchNames;
+    HashSet<String> commands;
     BufferedReader in;
 
-    private class Command{
+    private class Command{              //this class is used to modify command string in receive thread
         private String command;
 
         public String getCommand() {
@@ -41,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        switchNames = new HashSet<String>();
+        commands = new HashSet<String>();
+
         final Button scanButton = findViewById(R.id.scan_button);
 
         scanButton.setOnClickListener(new View.OnClickListener() {
@@ -52,16 +68,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
         ListView listView = (ListView) findViewById(R.id.listview_activity_main);
         switchList = new ArrayList<Switch>();
-        Switch s1 = new Switch("Kühlschrank", "-p clarus_switch -i A3 -u 20");
-        switchList.add(s1);
+
+        if(sharedPref.contains(switchKey) && sharedPref.contains(commandKey)) {
+            Iterator<String> switchNameIt = sharedPref.getStringSet(switchKey, null).iterator();
+            Iterator<String> commandIt = sharedPref.getStringSet(commandKey, null).iterator();
+            while (switchNameIt.hasNext() && commandIt.hasNext()) {
+                String switchName = switchNameIt.next();
+                String command = commandIt.next();
+                Switch s = new Switch(switchName, command);
+                switchList.add(s);
+
+                switchNames.add(switchName);
+                commands.add(command);
+            }
+        }
+
         switchAdapter = new SwitchAdapter(getApplicationContext(), switchList);
         listView.setAdapter(switchAdapter);
     }
 
     private void receiveMessage() throws InterruptedException {
-        //final TextView out = findViewById(R.id.output);
         TextInputLayout textInput = findViewById(R.id.name_input_layout);
         String switchName;
         switchName = textInput.getEditText().getText().toString();
@@ -90,6 +119,12 @@ public class MainActivity extends AppCompatActivity {
 
         thread.start();
         thread.join();
+        SharedPreferences.Editor editor = sharedPref.edit();
+        switchNames.add(switchName);
+        commands.add(com.getCommand());
+        editor.putStringSet(switchKey, switchNames);
+        editor.putStringSet(commandKey, commands);
+        editor.apply();
         switchAdapter.add(new Switch(switchName, com.getCommand()));
         switchAdapter.notifyDataSetChanged();
     }
