bdea6173f956f4821967a8b9a8ad90783f0452f5
Julian Brand <julian.brand@stud.hs-mannheim.de>
2021-03-18 13:32:35 +0100

    bugfix - save and access data which is send from server

--- AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/klugesheim/MainActivity.java
@@ -25,15 +25,13 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    static private String host = "";    //insert server ip here
+    static private String host = "192.168.2.200";    //insert server ip here
     static private int port = 2048;
-    private String switchKey = "switch_names";
-    private String commandKey = "commands";
+    private String listElDataKey = "data_key";
     ArrayList<Switch> switchList;
     SwitchAdapter switchAdapter;
     SharedPreferences sharedPref;
-    HashSet<String> switchNames;
-    HashSet<String> commands;
+    HashSet<String> listElData;
     BufferedReader in;
 
     private class Command{              //this class is used to modify command string in receive thread
@@ -54,8 +52,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
-        switchNames = new HashSet<String>();
-        commands = new HashSet<String>();
+        listElData = new HashSet<String>();
 
         final Button scanButton = findViewById(R.id.scan_button);
 
@@ -72,17 +69,15 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listview_activity_main);
         switchList = new ArrayList<Switch>();
 
-        if(sharedPref.contains(switchKey) && sharedPref.contains(commandKey)) {
-            Iterator<String> switchNameIt = sharedPref.getStringSet(switchKey, null).iterator();
-            Iterator<String> commandIt = sharedPref.getStringSet(commandKey, null).iterator();
-            while (switchNameIt.hasNext() && commandIt.hasNext()) {
-                String switchName = switchNameIt.next();
-                String command = commandIt.next();
+        if(sharedPref.contains(listElDataKey)){
+            Iterator<String> it = sharedPref.getStringSet(listElDataKey, null).iterator();
+            while(it.hasNext()){
+                String[] stringArray = it.next().split("/");
+                String switchName = stringArray[0];
+                String command = stringArray[1];
                 Switch s = new Switch(switchName, command);
                 switchList.add(s);
-
-                switchNames.add(switchName);
-                commands.add(command);
+                listElData.add(switchName + "/" + command);
             }
         }
 
@@ -120,10 +115,8 @@ public class MainActivity extends AppCompatActivity {
         thread.start();
         thread.join();
         SharedPreferences.Editor editor = sharedPref.edit();
-        switchNames.add(switchName);
-        commands.add(com.getCommand());
-        editor.putStringSet(switchKey, switchNames);
-        editor.putStringSet(commandKey, commands);
+        listElData.add(switchName + "/" + com.getCommand());
+        editor.putStringSet(listElDataKey, listElData);
         editor.apply();
         switchAdapter.add(new Switch(switchName, com.getCommand()));
         switchAdapter.notifyDataSetChanged();
